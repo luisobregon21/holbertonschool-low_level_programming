578323b9d4dfc265e130b42e95437cff6c454981
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-17 20:57:20 +0000

    stderr is standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function writes and formats the output to a stream.
+ * holds the message but not on stdout console.
+ * return: 1
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"- Dora korpar, 2015-10-19\n");
+	return (1);
+}
