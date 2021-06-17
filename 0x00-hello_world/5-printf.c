3a5117690e3fa4c2e18e269d1782523ddf9e5c5a
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-17 19:49:18 +0000

    used printf this time

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * used to print the character, string, float, integer,
+ * octal and hexadecimal value onto te screen.
+ * Use %(option) as a specifier.
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art \n");
+	return (0);
+}
