5e74c166efac82d948a89e2a559e6963ddb0c05d
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-17 20:22:28 +0000

    add (s)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - function
  * sizeof evaluate the size of a variable
@@ -11,9 +12,9 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of int: %zu bytes\n", sizeof(floatType));
-	printf("Size of int: %zu bytes\n", sizeof(doubleType));
-	printf("Size of int: %zu bytes\n", sizeof(charType));
+	printf("Size of int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of int: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of int: %zu byte(s)\n", sizeof(charType));
 	return (0);
 }
