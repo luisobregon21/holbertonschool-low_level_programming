bcf28d6e7f96d079009d112adf693a0f2fb1c37f
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-17 19:14:06 +0000

    changes made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) 
+/**
+ *main - function prints exact phrase
+ *Return: 0
+*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
