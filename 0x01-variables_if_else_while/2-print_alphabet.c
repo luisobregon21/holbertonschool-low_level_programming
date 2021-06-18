5576627234ffbc7355212118fd1b993a456d913e
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-18 23:05:02 +0000

    the value for lower case a is 97 and for z is 122

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function alphabetic order
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		putchar(a);
+			a++;
+	}
+	return (0);
+}
