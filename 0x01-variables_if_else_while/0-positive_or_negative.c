4a35ec7e8ca6a6787aad59ee93c9bfbd9c798c34
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-18 20:18:04 +0000

    The else if, is basically saying that if its not this if, then do this one.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - function
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive");
+	}
+	else if (n == 0)
+	{
+		printf("is zero");
+	}
+	else if (n < 0)
+	{
+		printf("is negative");
+	}
+	return (0);
+}
