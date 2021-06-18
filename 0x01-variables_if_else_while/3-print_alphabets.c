ba57e6487243a4c4deef71b33d1f817761e20594
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-18 23:24:02 +0000

    capital letters are from 65 - 90

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - function print all alphabet
+ * Return: 0
+*/
+
+int main(void)
+{
+	int a = 97;
+	int A = 65;
+
+	while (a <= 122)
+	{
+		putchar(a);
+			a++;
+	}
+	while (A <= 90)
+	{
+		putchar(A);
+			A++;
+	}
+	putchar('\n');
+}
