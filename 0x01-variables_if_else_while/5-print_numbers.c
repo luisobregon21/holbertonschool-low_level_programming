8916ec19dcc1920ec244224a16770e310389e2c8
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-19 00:13:48 +0000

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function prints single digits.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
