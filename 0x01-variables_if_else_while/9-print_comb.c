f356f15224b19cb646f9f6d4ea6663efe4c5d79b
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-19 01:08:04 +0000

    added , and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+*/
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		if (a != 57)
+		putchar(',');
+		putchar(' ');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
