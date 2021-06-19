7c44f9ca6f616890ca6d9e4e33a3da98a20771fe
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-19 02:22:24 +0000

    add task file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: 0
+*/
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 <= 7; num1++)
+	{
+		for (num2 = 1; num2 <= 8; num2++)
+		{
+			for (num3 = 2; num3 <= 9; num3++)
+			{
+				if (num1 < num2 && num2 < num3)
+				{
+					putchar(num1 + 48);
+					putchar(num2 + 48);
+					putchar(num3 + 48);
+					if (!(num1 == 8 && num2 == 8 && num3 == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
