f9be9c8f57743ff37a63d85ba1eb94e04f409782
Luis Obregon <luisitonicaragua2002@gmail.com>
2021-06-22 15:32:49 +0000

    added new file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - function prints out characters one by one
+ * this code will print out Holberton
+ * then will be followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
