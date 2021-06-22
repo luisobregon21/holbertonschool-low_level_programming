4c6092d72c65a7199fcf49637adf631caa3c2b3c
luis <luisitonicaragua2002@gmail.com>
2021-06-22 23:06:26 +0000

    added new file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - this is printing the min of day
+ * @void - int is being used
+ *
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
