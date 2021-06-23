8a110f6376cbc02466d06d00daffebdc8cca7b53
luis <luisitonicaragua2002@gmail.com>
2021-06-23 02:14:13 +0000

    added file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - function add two intergers
+ * @free: is a variable
+ * @bee: is a variable
+ * Return: sum of free and bee
+ */
+int add(int free, int bee)
+{
+	return (free + bee);
+}
