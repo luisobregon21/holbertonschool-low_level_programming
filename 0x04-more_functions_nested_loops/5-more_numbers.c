d352d3c3a529f9d358431cbca241ea80f1f8a7d0
luis <luisitonicaragua2002@gmail.com>
2021-06-24 20:18:47 +0000

    added new task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - function prints 0-14.
+ * it will do it 10 times.
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (b = 0; b <= 10; b++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a >= 10)
+			{
+				_putchar(a / 10 + '0');
+			}
+			_putchar(a % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
