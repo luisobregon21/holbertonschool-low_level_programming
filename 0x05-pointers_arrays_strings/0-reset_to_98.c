0c7b17d3db88db870308a02d9478c71e8577f8bc
luis <luisitonicaragua2002@gmail.com>
2021-06-28 16:37:15 +0000

    new task added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - takes pointer to an int.
+ * @n: is the parameter being used.
+ * @*n: is the pointer refereing to variable n.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
