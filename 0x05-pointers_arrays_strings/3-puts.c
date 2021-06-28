f5553b87c9b6049e87280819b9c668bd6de462a4
luis <luisitonicaragua2002@gmail.com>
2021-06-28 18:29:32 +0000

    new file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _puts - prints a string to stdout
+ * @str: variable for string
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+		_putchar(*str);
+	_putchar('\n');
+}
