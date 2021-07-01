68979d6ac66e60cd9373eba4f29d23889a85fa2e
luis <luisitonicaragua2002@gmail.com>
2021-07-01 15:19:51 +0000

    new task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcmp - compares two string.
+ * @s1: string.
+ * @s2: string2.
+ *
+ * Return: 0, negative or positive value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+		return (0);
+	else if (*s1 < *s2)
+		return (*s1 - *s2);
+	else
+		return (*s1 - *s2);
+}
