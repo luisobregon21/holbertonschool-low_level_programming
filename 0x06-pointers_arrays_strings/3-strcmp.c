833f063a6154a06a94238506d431e47c985b18d6
luis <luisitonicaragua2002@gmail.com>
2021-07-01 15:27:42 +0000

    changed order

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 == *s2)
-		return (0);
-	else if (*s1 < *s2)
+	if (*s1 < *s2)
 		return (*s1 - *s2);
+	else if (*s1 == *s2)
+		return (0);
 	else
 		return (*s1 - *s2);
+	return (0);
 }
