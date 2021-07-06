b9853de7c58519ae000ae18ed511bf4c8267c46c
luis <luisitonicaragua2002@gmail.com>
2021-07-06 17:36:03 +0000

    new task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stddef.h>
 
 /**
  * *_strchr - locates character in string.
@@ -16,5 +15,5 @@ char *_strchr(char *s, char c)
 		if (*s == c)
 			return (s);
 	}
-	return (NULL);
+	return (0);
 }
