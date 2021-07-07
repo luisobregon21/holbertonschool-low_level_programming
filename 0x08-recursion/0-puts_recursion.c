58b28af8c3a19ba32b765c2b7a9986530ce409ee
luis <luisitonicaragua2002@gmail.com>
2021-07-07 15:43:35 +0000

    new task added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - prints a string followed by '\n'.
+ * @s: string.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
