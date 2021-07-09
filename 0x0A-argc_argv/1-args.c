fbb12af2bf7205f4d5c37ff18a4a9169f75cda2e
luis <luisitonicaragua2002@gmail.com>
2021-07-09 16:08:00 +0000

    new task, used void for argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguements passed into it.
+ * @argc: argument count
+ * @argv: arguement vector.
+ *
+ * Return: print number followed by new line.
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
