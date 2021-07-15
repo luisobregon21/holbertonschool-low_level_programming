45bedc77847da876b148919b2b2d9efd3dbd3a84
luis <luisitonicaragua2002@gmail.com>
2021-07-15 18:56:51 +0000

    added stdlib.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,5 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdlib.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+
+#endif /*_HOLBERTON_H_*/
