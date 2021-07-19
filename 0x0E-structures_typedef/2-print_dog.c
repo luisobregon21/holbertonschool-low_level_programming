16f8805674a9db55013b576664242b86aae51ad7
luis <luisitonicaragua2002@gmail.com>
2021-07-19 20:37:09 +0000

    new task

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * print_dog - prints struct dog.
+ * @d: pointer to the dog.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL || (*d).name == NULL)
+		printf("nil\n");
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
