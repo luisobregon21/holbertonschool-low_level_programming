61f4b0c764b807bafe247f561b7f8b15d81071d4
luis <luisitonicaragua2002@gmail.com>
2021-07-19 20:39:59 +0000

    new task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,10 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL || (*d).name == NULL)
+	{
 		printf("nil\n");
+		return;
+	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
