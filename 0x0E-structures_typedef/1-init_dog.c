ab658024b073b088d5a7d40dbf16e9a3c945cd27
luis <luisitonicaragua2002@gmail.com>
2021-07-19 20:01:56 +0000

    new task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - initializes the dog
+ * @d: dog
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == 0)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
