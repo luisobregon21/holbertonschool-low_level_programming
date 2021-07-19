be45259cb376e2fd4d05b6b0ffc7dfb503b8be1f
luis <luisitonicaragua2002@gmail.com>
2021-07-19 21:13:50 +0000

    new task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,7 @@ struct dog
 	char *owner;
 };
 /*the prototypes are down below*/
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG */
