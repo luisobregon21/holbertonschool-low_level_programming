1e173c1d0010f588a4f32546bfa0f0442b40db66
luis <luisitonicaragua2002@gmail.com>
2021-07-19 22:33:03 +0000

    new task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,76 @@
+#include "dog.h"
+char *_strcpy(char *s1, char *s2);
+unsigned int _strlen(char *s);
+/**
+ * *new_dog - creates a new dog
+ * @name: name of new dog.
+ * @age: age of new dog.
+ * @owner: name of owner.
+ * Return: new dog.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+	char *name_cpy;
+	char *owner_cpy;
+
+	if (name == NULL)
+		return (NULL);
+	if (owner == NULL)
+		return (NULL);
+
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
+
+	name_cpy = malloc(_strlen(name) + 1);
+	if (name_cpy == NULL)
+	{
+		free(doggo);
+		return (NULL);
+	}
+
+	owner_cpy = malloc(_strlen(name) + 1);
+	if (owner_cpy == NULL)
+	{
+		free(doggo);
+		free(name_cpy);
+		return (NULL);
+	}
+
+	_strcpy(name, name_cpy);
+	_strcpy(owner, owner_cpy);
+
+	doggo->name = name;
+	doggo->age = age;
+	doggo->owner = owner;
+
+	return (doggo);
+}
+/**
+ * *_strcpy - copies one string to another
+ * @s1: string being copied
+ * @s2: copy of the first string
+ * Return: new string
+*/
+char *_strcpy(char *s1, char *s2)
+{
+	int idx;
+
+	for (idx = 0; s1[idx] != '\0'; idx++)
+		s2[idx] = s1[idx];
+	return (s2);
+}
+/**
+ * _strlen - finds string length
+ * @s: string
+ * Return: the size of the string.
+*/
+unsigned int _strlen(char *s)
+{
+	unsigned int size = 0;
+
+	while (s[size] != '\0')
+		size++;
+	return (size);
+}
