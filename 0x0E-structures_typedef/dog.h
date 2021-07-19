d97d803c40e10169d8a8ee56b59f59209e9cda93
luis <luisitonicaragua2002@gmail.com>
2021-07-19 19:32:46 +0000

    new task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - basic dog descript.
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG */
