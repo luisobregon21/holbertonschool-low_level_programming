fa6b835309708b4b3975fec1c06faf4407c3997e
luis <luisitonicaragua2002@gmail.com>
2021-07-21 18:47:33 +0000

    new task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name;
+ * @name: string of the name given.
+ * @f: pointer to a function that has a string.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
