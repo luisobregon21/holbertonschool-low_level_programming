e50f20fb02ac8c3b55a7412a2586f4eed31c2840
luis <luisitonicaragua2002@gmail.com>
2021-07-22 00:37:17 +0000

    changed if and exit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if (calc == / || calc == %)
+	if ((*argv[2] == '/' || *argv[2] == '%') && argv[3] == 0)
 	{
 		printf("Error\n");
 		exit(100);
