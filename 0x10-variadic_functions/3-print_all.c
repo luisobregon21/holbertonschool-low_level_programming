b6b53c203f2f6f6b2200628a4599d0910b8566b9
luis <luisitonicaragua2002@gmail.com>
2021-07-23 03:17:10 +0000

    new task

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -36,7 +36,7 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(list, char *);
 				if (str == NULL)
-					printf("(nil)");
+					str = ("(nil)");
 				printf("%s%s", comma, str);
 		}
 		comma = ", ";
