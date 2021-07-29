f8e1e44611da0cc4216d4982795688b140a6ff99
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-07-29 18:23:44 +0000

    new prototype added

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: is of the follwing types...
+ * char *str
+ * unsigned int len
+ * struct list_s *next
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+}
