d13b187eada7715776f62c7a48c07faee9faf63d
luis <luisitonicaragua2002@gmail.com>
2021-07-29 21:26:06 +0000

    added new line to printf and added an else statement

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,4 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
+	const list_t *tmp = h;
+	int count = 0;
+
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+			printf("[%i] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
+		count++;
+	}
+	return (count);
 }
