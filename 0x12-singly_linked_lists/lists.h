7d754038d37b9409f3fc8da8b671f7a414307513
luis <luisitonicaragua2002@gmail.com>
2021-07-29 21:24:56 +0000

    added new prototype

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,14 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
-int _putchar(char c);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -25,4 +17,12 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 #endif /*LISTS_H*/
