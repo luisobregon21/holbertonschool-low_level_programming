1ddaadae116964d73a8417a54d83e95f5ca3761d
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-08-04 00:41:12 +0000

    took if statement out of the loop

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int counter = 1;
 
 	if (head == NULL)
-		return (NULL);
+		return (0);
 
 	tmp = *head;
 	if (idx == 0)
