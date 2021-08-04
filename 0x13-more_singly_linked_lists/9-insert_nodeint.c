4324c6b6c6267bbe54ffa45e56aff5a5a9973e41
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-08-04 00:32:27 +0000

    added a create node function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new = NULL, *tmp;
 	unsigned int counter = 1;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (NULL);
 
 	tmp = *head;
