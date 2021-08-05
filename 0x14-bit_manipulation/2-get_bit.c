800e02c7246e4022f921ab9ffc160406f4b3befa
luis <luisitonicaragua2002@gmail.com>
2021-08-05 22:35:48 +0000

    new task

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * get_bit - returns value of bit at given index.
+ * @index: place where bit will be placed.
+ * @n: value of bit.
+ * Return: bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int count = 0;
+	int bit;
+
+	while (count < index)
+	{
+		n = n >> 1;
+		count++;
+	}
+	bit = n & 1;
+
+	return (bit);
+}
