944444776d8d8c7b308baf4c81b1820b20c19c45
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-08-31 21:31:19 +0000

    Fixed betty complaints

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_number - prints an integer.
@@ -7,8 +7,14 @@
 
 void print_number(int n)
 {
-	if ( n / 10)
+	if (n < 0)
 	{
-	print_number(n / 10);
+		_putchar('-');
+		n = n * (-1);
 	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
 }
