d382c2e2776ca22e67c48c0d97254552a773be96
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-09-17 20:53:10 +0000

    Add new task

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
