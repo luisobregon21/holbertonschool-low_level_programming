6a0f4e17b5e7a0de6d896d190e64a4de4113e4a5
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-08 22:58:37 +0000

    Add conditional statement

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,7 +8,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node = NULL;
+	hash_node_t *node = NULL, *tmp;
 	unsigned long int idx = 0;
 
 	if (key == NULL)
@@ -24,7 +24,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->key = (char *) key;
 	node->value = (char *) value;
 	idx = key_index((const unsigned char *) key, ht->size);
-	ht->array[idx] = node;
 
+	if (ht->array[idx] == NULL)
+	{
+		ht->array[idx] = node;
+	}
+	else
+	{
+		tmp = ht->array[idx];
+		while (tmp != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp = ht->array[idx];
+		node->next = tmp;
+		ht->array[idx] = node;
+	}
 	return (1);
 }
