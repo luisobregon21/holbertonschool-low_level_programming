f59f8ed01b5dcd7b14a5790dea8ccae3f08d611e
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-08 23:51:27 +0000

    Add new task

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - print hash table
+ * @ht: the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	char *faker = "";
+	
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	while (idx < ht->size)
+	{
+		while (ht->array[idx] != NULL)
+		{
+			printf("%s'%s': '%s'", faker, ht->array[idx]->key, ht->array[idx]->value);
+			faker = ", ";
+			ht->array[idx] = ht->array[idx]->next;
+		}
+		idx++;
+	}
+	printf("}\n");
+}
