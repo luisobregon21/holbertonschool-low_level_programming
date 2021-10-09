d30eb49689db3e397a159933bc67c9d1869aa4d3
luisobregon21 <luisitonicaragua2002@gmail.com>
2021-10-09 00:10:04 +0000

    Add new task

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - frees hash table
+ * @ht: the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t *tmp;
+
+	if (ht == NULL)
+		return;
+
+	if (ht->array == NULL)
+	{
+		free(ht->array);
+		return;
+	}
+
+	while (idx < ht->size)
+	{
+		tmp = ht->array[idx];
+		while (tmp != NULL)
+		{
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = tmp->next;
+		}
+		idx++;
+	}
+	free(ht->array);
+	free(ht);
+}
